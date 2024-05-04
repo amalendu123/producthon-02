45578b9795e6244070548a89e747a21a045a5e4e
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-05-04 10:58:05 +0530

    added appbar

--- appcode/lib/Homescreen/Homescreen.dart
+++ appcode/lib/Homescreen/Homescreen.dart
@@ -6,8 +6,42 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
-        title: const Text('Home'),
+        leading:Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.location_city),
+              onPressed: () {
+                // Do something
+              },
+            ),
+            Text("Kollam")
+          ],
+        ),
+
+        leadingWidth: 100,
+        title:Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              // Do something
+            },
+          ),
+         
+        ],
+      ),
+        backgroundColor: Colors.blue,
+      ),
+      body: const SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Parking Near You",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+          ]
+        ),
       ),
     );
   }
